f5484ca62a7d4e615911d1d792662044bdf6d807
developer119 <developer119korea@gmail.com>
2017-11-25 20:00:05 +0900

    스크롤뷰 패널을 만들고 그안에 5개의 버튼을 만들었다. 센터포지션 좌표를 비교하기 위한 객체 만들었고 스크롤뷰를 컨트롤하기위한 객체 및 스크립트 추가했다
    센터포지션으로부터 각 버튼들의 거리를 계산하는 코드 생성

---
+++ Assets/Scripts/ScrollRectSnap_CS.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollRectSnap_CS : MonoBehaviour 
+{
+
+    public RectTransform panel;
+    public Button[] bttn;
+    public RectTransform center;
+
+    private float[] distance;
+    private bool dragging = false;
+    private int bttnDistance;
+    private int minButtonNum;
+
+    private void Start()
+    {
+        int bttnLength = bttn.Length;
+        distance = new float[bttnLength];
+
+        bttnDistance = (int)Mathf.Abs(bttn[1].GetComponent<RectTransform>().anchoredPosition.x - bttn[0].GetComponent<RectTransform>().anchoredPosition.x);
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < bttn.Length; i++)
+        {
+            distance[i] = Mathf.Abs(center.transform.position.x - bttn[i].transform.position.x);
+        }
+    }
+
+}
